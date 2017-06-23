f9239cd573d17c4d42b15c4062e31b3b91704ed4
MRROBOT\nicai <nicaiczy1996@gmail.com>
2017-06-23 14:58:18 +0800

    tokenizer

--- src/ast.py
+++ src/ast.py
@@ -2,11 +2,12 @@ from utils import log
 
 
 def string_element(s):
-    r = ''
+    r = '"'
     ec = False
     for i, e in enumerate(s[1:]):
         r += e
         if ec:
+            ec = False
             continue
         elif e == '\\':
             ec = True
@@ -18,9 +19,25 @@ def string_element(s):
 def common_element(s):
     for i, e in enumerate(s):
         if e == '"':
-            return string_element[i:]
+            return string_element(s[i:])
         elif e in ') ':
             return s[:i]
+
+
+def formatted_token(token):
+    '''
+    数字转换为数字，替他不变
+    :param token:
+    :return:
+    '''
+    num = '012345678'
+    if token[0] in num:
+        if '.' in token:
+            return float(token)
+        else:
+            return int(token)
+    else:
+        return token
 
 
 def tonkenizer(s):
@@ -35,9 +52,11 @@ def tonkenizer(s):
         elif e == ' ':
             pass
         else:
-            ele = common_element(s[i:])
-            count = len(ele) - 1
-            l.append(ele)
+            token = common_element(s[i:])
+            count = len(token) - 1
+            token = formatted_token(token)
+            l.append(token)
+    return l
 
 
 def test_common_element():
@@ -47,5 +66,18 @@ def test_common_element():
     log(common_element(st2))  # bar
 
 
+def test_tokenizer():
+    s1 = '(+ 1 2 (- 3 4))'
+    s2 = '(+ 12 2.34 (- 345 45))'
+    s3 = '(+ foo 2.34 (- 3 bar))'
+    s4 = '(+ foo 2.34 (- 3 "hi(\\" )"))'
+
+    log(s1, tonkenizer(s1))
+    log(s2, tonkenizer(s2))
+    log(s3, tonkenizer(s3))
+    log(s4, tonkenizer(s4))
+
+
 if __name__ == '__main__':
     test_common_element()
+    test_tokenizer()
